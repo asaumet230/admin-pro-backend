8a87ea87691e2818edc66e7c1757d12d3ea64a16
Andres Felipe Saumet <andressaumet@gmail.com>
2021-10-18 10:56:36 -0500

    Arreglo del path index SPA

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const path= require('path');
 require("dotenv").config();
 
 const { conexionDb }= require('./dataBase/config');
@@ -11,6 +12,10 @@ conexionDb();
 
 // Habilitar Cors:
 app.use(cors());
+/** Nota aqui no tienes que restringir el acceso a la API porque todo esta en el mismo lugar, en los 
+ * otros despliegues que hemos hecho con react el frontend estaba en Netlify y el backend en Heroku
+ * pro esto habia que restringir el acceso a la API
+ */
 
 // Habilitar body parser
 app.use( express.json() );
@@ -27,6 +32,15 @@ app.use('/api/login', require('./routes/auth'));
 app.use('/api/busquedas', require('./routes/busquedas'));
 app.use('/api/uploads', require('./routes/uploads'));
 
+
+/* 
+Eto es para obligar que mi SPA al recargar todas las rutas deban pasar por el index.html 
+Ojo esto solo aplica si es una single page aplication o SPA, no si usas angular universal
+o en el caso de react next o gatsby
+*/
+app.get('*', (req, res) => {
+    res.sendFile( path.resolve( __dirname, 'public/index.html') );
+});
 
 
 app.listen(process.env.PORT, ()=> {
